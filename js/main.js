dd589a320aca52069eed0bd760c26fae55f3b984
Jacob Husband <jakehusband2@gmail.com>
2022-10-14 15:42:06 +0000

    Listen to nav bar clicks

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$navBar = document.querySelector(".nav-bar");
+
+$navBar.addEventListener("click", handleNavBarClicks);
+
+function handleNavBarClicks(event) {
+  if (event.target.matches(".hamburger-icon")) {
+  }
+  if (event.target.matches(".search-icon")) {
+  }
+}
