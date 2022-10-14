3990094ee11227ac0c515a19395adc8adf5170cf
Jacob Husband <jakehusband2@gmail.com>
2022-10-14 19:17:38 +0000

    Add hamburger menu animation

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
 $navBar = document.querySelector(".nav-bar");
+$hamburgerModal = document.querySelector(".hamburger-menu-container");
+$hamburgerMenu = $hamburgerModal.firstElementChild;
 
 $navBar.addEventListener("click", handleNavBarClicks);
+$hamburgerMenu.addEventListener("click", handleHamburgerMenuClicks);
 
 function handleNavBarClicks(event) {
   if (event.target.matches(".hamburger-icon")) {
+    $hamburgerModal.classList.remove("hidden");
+    setTimeout(() => {
+      $hamburgerMenu.style.transform = `translateX(12rem)`;
+    }, 10);
   }
   if (event.target.matches(".search-icon")) {
+  }
+}
+
+function handleHamburgerMenuClicks(event) {
+  if (event.target.matches(".hamburger-exit")) {
+    $hamburgerMenu.style.transform = `translateX(0)`;
+    setTimeout(() => {
+      $hamburgerModal.classList.add("hidden");
+    }, 250);
   }
 }
