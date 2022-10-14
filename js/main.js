953c1d346dda85f4875af84fde2fad7ec4468a53
Jacob Husband <jakehusband2@gmail.com>
2022-10-14 21:56:05 +0000

    Add search animation

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 $navBar = document.querySelector(".nav-bar");
 $hamburgerModal = document.querySelector(".hamburger-menu-container");
 $hamburgerMenu = $hamburgerModal.firstElementChild;
+$navForm = $navBar.querySelector("form");
+$searchIcon = $navBar.querySelector(".search-icon");
+$searchInput = $navBar.querySelector("input");
+$searchX = $navBar.querySelector(".search-x");
 
 $navBar.addEventListener("click", handleNavBarClicks);
 $hamburgerMenu.addEventListener("click", handleHamburgerMenuClicks);
+$navForm.addEventListener("submit", handleNavFormSubmits);
 
 function handleNavBarClicks(event) {
   if (event.target.matches(".hamburger-icon")) {
@@ -13,6 +18,19 @@ function handleNavBarClicks(event) {
     }, 10);
   }
   if (event.target.matches(".search-icon")) {
+    $searchIcon.style.transform = `translateX(-12.5rem)`;
+    setTimeout(() => {
+      $searchInput.classList.remove("hidden");
+      $searchX.classList.remove("hidden");
+      $searchInput.focus();
+    }, 400);
+  }
+  if (event.target.matches(".search-x")) {
+    $searchX.classList.add("hidden");
+    $searchIcon.style.transform = `translateX(0)`;
+    setTimeout(() => {
+      $searchInput.classList.add("hidden");
+    }, 10);
   }
 }
 
@@ -23,4 +41,8 @@ function handleHamburgerMenuClicks(event) {
       $hamburgerModal.classList.add("hidden");
     }, 250);
   }
+}
+
+function handleNavFormSubmits(event) {
+  event.preventDefault();
 }
