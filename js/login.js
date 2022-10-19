1b662cfb6cc6bfc2c7e6cddec06e7fa5ef7afd26
Jacob Husband <jakehusband2@gmail.com>
2022-10-19 18:37:05 +0000

    Add form submit to login

--- js/login.js
+++ js/login.js
@@ -1,1 +1,12 @@
-console.log("hi");
+$form = document.querySelector("form");
+
+$form.addEventListener("submit", handleLogin);
+
+function handleLogin(event) {
+  event.preventDefault();
+  const form = event.target.elements;
+  const username = form[0].value;
+  const password = form[1].value;
+  if (username === "blah" && password === "blah") {
+  }
+}
