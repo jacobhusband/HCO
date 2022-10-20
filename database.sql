a500f8e112185cd7b00c8dbef6573d5ff19b965f
Jacob Husband <jakehusband2@gmail.com>
2022-10-20 19:12:27 +0000

    Add tables and items into hco database

--- database.sql
+++ database.sql
@@ -1,11 +1,24 @@
 CREATE TABLE products (
-    id SERIAL NOT NULL PRIMARY KEY,
+    productId INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
-    price INT NOT NULL
+    price INT NOT NULL,
+    PRIMARY KEY(productId)
 );
 
-INSERT INTO "products" ("name", "description", "price")
-VALUES ('grey sofa', 'old but good', 300),
-       ('blue sofa', 'stinky smelly', 400)
-RETURNING *;
+CREATE TABLE images (
+    imageId INT GENERATED ALWAYS AS IDENTITY,
+    productId INT,
+    url VARCHAR(255) NOT NULL,
+    PRIMARY KEY(imageId),
+    CONSTRAINT fk_product
+        FOREIGN KEY(productId)
+            REFERENCES products(productId)
+            ON DELETE CASCADE
+);
+
+INSERT INTO products(name, description, price)
+VALUES ('Grey Sofa', 'Old and raggity sofa', 200);
+
+INSERT INTO images(productId, url)
+VALUES (1, 'https://secure.img1-fg.wfcdn.com/im/66690414/compr-r85/1287/128797044/erinn-6725-upholstered-loveseat.jpg');
--- js/server.js
+++ js/server.js
@@ -1,16 +1,23 @@
 const express = require("express");
+const pg = require("pg");
+const pool = new Pool();
 const app = express();
 const cors = require("cors");
 const path = require("path");
 
-//middleware
+const db = new pg.Pool({
+  connectionString: "postgres://dev:dev@localhost/studentGradeTable",
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "../styles")));
 
-app.get("/api/:id", (req, res, next) => {
-  console.log(req.params.id);
+app.get("/api/db", (req, res, next) => {
   res.status(200);
   res.json({ id: req.params.id });
   next();
