3c942dfe315591a0b1a04d74e8ccbe8555d7b8fe
Jacob Husband <jakehusband2@gmail.com>
2022-12-05 22:40:27 +0000

    Change footer style and add more specificity to inventory card styling

--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -6,13 +6,13 @@ export default function Footer(props) {
   const footerClass = (props.width) ? '' : 'footer'
 
   return (
-    <Nav className='justify-content-around bg-primary p-3 mt-auto'>
+    <Nav className='justify-content-around bg-light p-3 mt-auto'>
       <Container className={`${footerClass} d-flex justify-content-around`}>
-            <a className='text-light text-decoration-none' data-link="homepage" href="#">Home</a>
-            <a className='text-light text-decoration-none' data-link="inventory" href="#inventory">Inventory</a>
-            <a className='text-light text-decoration-none' data-link="about" href="#about">About</a>
-            <a className='text-light text-decoration-none' data-link="contact" href="#contact">Contact</a>
-            <a className='text-light text-decoration-none' data-link="faq" href="#faq">FAQ</a>
+            <a className='text-dark text-decoration-none' data-link="homepage" href="#">Home</a>
+            <a className='text-dark text-decoration-none' data-link="inventory" href="#inventory">Inventory</a>
+            <a className='text-dark text-decoration-none' data-link="about" href="#about">About</a>
+            <a className='text-dark text-decoration-none' data-link="contact" href="#contact">Contact</a>
+            <a className='text-dark text-decoration-none' data-link="faq" href="#faq">FAQ</a>
       </Container>
     </Nav>
   )
